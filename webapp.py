8728ad9b368d8d53eb171ff1f7d5d5d3033b74b5
Luca <lucaccorey@gmail.com>
2020-02-20 09:36:23 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -72,14 +72,6 @@ def authorized():
     return render_template('message.html', message=message)
 
 
-@app.route('/page1')
-def renderPage1():
-    if 'user_data' in session:
-        user_data_pprint = pprint.pformat(session['user_data'])#format the user data nicely
-    else:
-        user_data_pprint = '';
-    return render_template('page1.html',dump_user_data=user_data_pprint)
-
 
 @github.tokengetter
 def get_github_oauth_token():
